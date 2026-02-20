339ac939923769ea044df1280630dd8b5f427a33
shishirbohara <shishirbohara68@gmail.com>
2026-02-20 23:01:07 +0545

    remove trailing slash

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://testing-prod-bice.vercel.app/'],
+    origin: ['https://testing-prod-bice.vercel.app'],
     credentials: true,
     methods: 'GET,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
