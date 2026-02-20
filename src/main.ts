be80e69c20ed2b402ea8c5e21aa5f9e4664ba154
shishirbohara <shishirbohara68@gmail.com>
2026-02-20 22:50:10 +0545

    add frontend url

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://testing-prod-bice.vercel.app/'],
     credentials: true,
     methods: 'GET,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
-  
+
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
